a858afb7b6e120b8d07bf063b1c50f3aa31dd0ce
Théo Ryffel <theo.leffyr@gmail.com>
2019-11-19 16:22:36 +0100

    Update map_deceased.py

--- scripts/custom/map_deceased.py
+++ scripts/custom/map_deceased.py
@@ -8,6 +8,7 @@ class Deceased(Enum):
 
 
 def map_deceased(raw_input):
+    """Map deceased code to boolean 'O'->True 'N'->False"""
     mapping = {"O": Deceased.DEAD.value, "N": Deceased.ALIVE.value}
     if raw_input in mapping.keys():
         return mapping[raw_input]
