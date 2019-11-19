7b87e7d0b75bb31c2f36c36cb33fcb2ad54a8114
Théo Ryffel <theo.leffyr@gmail.com>
2019-11-19 16:20:11 +0100

    Update clean_address.py

--- scripts/custom/clean_address.py
+++ scripts/custom/clean_address.py
@@ -1,5 +1,5 @@
 def process_address(raw_input):
-    """Clean you addresses!"""
+    """Clean your addresses"""
     if raw_input is None:
         return None
     # TODO: Process: add coma, R->RUE, etc.
