ef96b594b0d0d3c5cd402c3797391965b619a362
Théo Ryffel <theo.leffyr@gmail.com>
2019-11-19 16:26:04 +0100

    Update merge_insee.py

--- scripts/custom/merge_insee.py
+++ scripts/custom/merge_insee.py
@@ -2,6 +2,7 @@ from scripts import utils
 
 
 def merge_insee(value1, value2):
+    """Custom script (x, y) -> x if x is not None else y"""
     if not utils.is_empty(value1):
         insee = value1
     else:
