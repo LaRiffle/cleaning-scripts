9641b754f93637e5bf79142a3e07e46de96f31bf
Théo Ryffel <theo.leffyr@gmail.com>
2019-11-19 16:21:22 +0100

    Update clean_phone.py

--- scripts/custom/clean_phone.py
+++ scripts/custom/clean_phone.py
@@ -2,6 +2,7 @@ import re
 
 
 def clean_phone(raw_input):
+    """Clean phone number to 0x xx xx xx xx"""
     pattern = re.compile("(\d{2})" + "[\.\-\s]*(\d{2})" * 4)
     occurrences = pattern.findall(raw_input)
     if len(occurrences) > 0:
